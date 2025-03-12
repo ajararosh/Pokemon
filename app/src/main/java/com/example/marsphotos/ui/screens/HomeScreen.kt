5f74f1f9ccd342576286fbbf1bd352ed63993f06
ajararosh <ajara@roshka.com>
2025-03-12 15:43:34 -0300

    Changing app title

--- app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/HomeScreen.kt
@@ -133,7 +133,7 @@ fun MarsPhotoCard(photo: MarsPhoto, modifier: Modifier = Modifier) {
                 .crossfade(true).build(),
             error = painterResource(R.drawable.ic_broken_image),
             placeholder = painterResource(R.drawable.loading_img),
-            contentDescription = stringResource(R.string.mars_photo),
+            contentDescription = stringResource(R.string.pokemon_photo),
             contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxWidth()
         )
